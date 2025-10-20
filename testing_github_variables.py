77ba9f1323380a8a9f99ca71505fec3e81dfdac0
GANESH CHANDRA <77651439+M-G-C-64@users.noreply.github.com>
2025-10-21 00:56:24 +0530

    Update testing_github_variables.py

--- testing_github_variables.py
+++ testing_github_variables.py
@@ -1,5 +1,8 @@
 import os
 
-print("trying to access the keys")
+print("trying to access the keys for aqi_api_key")
 print(os.getenv('aqi_api_key'))
+print("didint work")
+print(os.getenv('AQI_API_KEY'))
+
 
